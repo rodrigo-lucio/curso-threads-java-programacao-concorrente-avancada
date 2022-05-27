0099fcbb9f7458e60c9d30ea2efcf9b24138252e
Rodrigo Lúcio <luciodigo@gmail.com>
2022-05-27 19:00:31 -0300

    Implementação do servidor

---
+++ src/main/java/br/com/lucio/cliente/ClienteTarefas.java
@@ -0,0 +1,65 @@
+package br.com.lucio.cliente;
+
+import java.io.IOException;
+import java.io.PrintStream;
+import java.net.Socket;
+import java.util.Scanner;
+
+public class ClienteTarefas {
+
+    public static void main(String[] args) throws Exception {
+
+        Socket socket = new Socket("localhost", 12346);
+        System.out.println("Conexão estabelecida");
+
+        Thread enviaComando = new Thread(() -> {
+
+            PrintStream saida;
+            try {
+                saida = new PrintStream(socket.getOutputStream());
+
+                Scanner teclado = new Scanner(System.in);
+
+                System.out.println("Informe uma mensagem para enviar para o servidor:");
+                while (teclado.hasNextLine()) {
+                    String linha = teclado.nextLine();
+                    if (linha.trim().equals("")) {
+                        break;
+                    }
+                    saida.println(linha);
+                }
+
+                saida.close();
+                teclado.close();
+
+            } catch (IOException e) {
+                throw new RuntimeException(e);
+            }
+
+        }); 
+
+        Thread recebeRespostaServidor = new Thread(() -> {
+            System.out.println("Obtendo resposta do servidor:");
+            Scanner respostaServidor;
+            try {
+                respostaServidor = new Scanner(socket.getInputStream());
+
+                while (respostaServidor.hasNextLine()) {
+                    System.out.println(respostaServidor.nextLine());
+                }
+
+            } catch (IOException e) {
+                throw new RuntimeException(e);
+            }
+        });
+
+        enviaComando.start();
+        recebeRespostaServidor.start();
+        
+        //Dizemos para a Thread main se juntar a essa thread envia comando, e só após ela encerrar, o socket.close é executado
+        enviaComando.join();
+        System.out.println("Conexão encerrada");
+        socket.close();
+
+    }
+}
